037884c56c5e723383e6503464536a743c267a43
Tuğsem Saranlı <saranlitugsem@gmail.com>
2022-06-25 01:11:22 +0300

    Revert "Air Pollution App: React-Capstone"

--- src/utils/fetchApi.js
+++
@@ -1,8 +0,0 @@
-const getDetail = async (lat, lon) => {
-  const response = await
-  fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=2f9f94ed8b19efaa15c726bd6e401398`);
-  const data = response.json();
-  return data;
-};
-
-export default getDetail;
