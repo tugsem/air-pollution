08280844bf3d17ab3955120cf4eee1f8c5ce7e63
Tugsem <saranlitugsem@gmail.com>
2022-06-25 00:57:38 +0300

    Deploy with gh-pages

--- src/__tests__/cityCard.test.js
+++ src/__tests__/cityCard.test.js
@@ -1,13 +1,14 @@
 import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
 import CityCard from '../components/cityCard';
-import { BrowserRouter as Router} from 'react-router-dom';
 
-it("renders correctly", () => {
+it('renders correctly', () => {
   const tree = renderer
     .create(
-    <Router>
+      <Router>
         <CityCard />
-    </Router>)
+      </Router>,
+    )
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
